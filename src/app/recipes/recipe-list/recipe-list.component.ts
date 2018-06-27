69c0f7f926d4872e40f8e8d9e206d4e3741b8a96
Ammar Aljuboori <AljubooriA@michigan.gov>
2018-06-27 12:59:22 -0400

    Displaying Recipe Details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import {Recipe} from '../recipe.model';
 export class RecipesListComponent implements OnInit {
 
   recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg'), 
     new Recipe('A test recipe', 'this is a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg')
   ];
 
